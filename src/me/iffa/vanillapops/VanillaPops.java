d20f976acd70d5572b4e15a0e106e6b72e77b68e
Santtu Elo <iffamies1337@hotmail.fi>
2011-10-02 23:37:50 +0300

    Added 3 populators from BGen - DungeonPopulator - FlowerPopulator - SnowPopulator

--- src/me/iffa/vanillapops/VanillaPops.java
+++ src/me/iffa/vanillapops/VanillaPops.java
@@ -3,6 +3,9 @@ package me.iffa.vanillapops;
 
 // VanillaPops Imports
 import me.iffa.vanillapops.populators.BedrockPopulator;
+import me.iffa.vanillapops.populators.DungeonPopulator;
+import me.iffa.vanillapops.populators.FlowerPopulator;
+import me.iffa.vanillapops.populators.SnowPopulator;
 import me.iffa.vanillapops.populators.nether.GlowstonePopulator;
 
 /**
@@ -13,7 +16,46 @@ import me.iffa.vanillapops.populators.nether.GlowstonePopulator;
 public class VanillaPops {
     // Variables - TODO: Place populator variables here.
     private BedrockPopulator bedrockPopulator = new BedrockPopulator();
+    private FlowerPopulator flowerPopulator = new FlowerPopulator();
+    private SnowPopulator snowPopulator = new SnowPopulator();
+    private DungeonPopulator dungeonPopulator = new DungeonPopulator();
     private GlowstonePopulator glowstonePopulator = new GlowstonePopulator();
+
+    /**
+     * Gets the bedrock populator.
+     * 
+     * @return Bedrock populator
+     */
+    public BedrockPopulator getBedrockPopulator() {
+        return this.bedrockPopulator;
+    }
+    
+    /**
+     * Gets the dungeon populator.
+     * 
+     * @return Dungeon populator
+     */
+    public DungeonPopulator getDungeonPopulator() {
+        return this.dungeonPopulator;
+    }
+    
+    /**
+     * Gets the flower populator.
+     * 
+     * @return Flower populator
+     */
+    public FlowerPopulator getFlowerPopulator() {
+        return this.flowerPopulator;
+    }
+    
+    /**
+     * Gets the snow populator.
+     * 
+     * @return Snow populator
+     */
+    public SnowPopulator getSnowPopulator() {
+        return this.snowPopulator;
+    }
 
     /**
      * Gets the glowstone populator.
@@ -22,14 +64,5 @@ public class VanillaPops {
      */
     public GlowstonePopulator getGlowstonePopulator() {
         return this.glowstonePopulator;
-    }
-    
-    /**
-     * Gets the bedrock populator.
-     * 
-     * @return Bedrock populator
-     */
-    public BedrockPopulator getBedrockPopulator() {
-        return this.bedrockPopulator;
     }
 }
